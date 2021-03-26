23a88edd40b3664919f96ed29c4e315935a2f1fd
kiran stha <kiranstha68@gmail.com>
2021-03-27 00:26:39 +0545

    Updates

---
+++ precache-manifest.070fa1dcca5652621ffbb75693230158.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5573777673cbeccf81c4a5454d9594",
+    "url": "/1st-website/index.html"
+  },
+  {
+    "revision": "20dbc699bca29de3959f",
+    "url": "/1st-website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "6ea2975d8844c5ab22ea",
+    "url": "/1st-website/static/css/main.8f29048e.chunk.css"
+  },
+  {
+    "revision": "20dbc699bca29de3959f",
+    "url": "/1st-website/static/js/2.e75a1a9b.chunk.js"
+  },
+  {
+    "revision": "4f89695379d3cca452a09d6ce7984607",
+    "url": "/1st-website/static/js/2.e75a1a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea2975d8844c5ab22ea",
+    "url": "/1st-website/static/js/main.9f67ec91.chunk.js"
+  },
+  {
+    "revision": "1e2d6c6b71d57d59ba03",
+    "url": "/1st-website/static/js/runtime-main.5e041f00.js"
+  },
+  {
+    "revision": "82a51cefe8e2c0cb1d05283c85f2b1e7",
+    "url": "/1st-website/static/media/Android.82a51cef.jpg"
+  },
+  {
+    "revision": "4117af083b5ab1dac6bd87d17af74eb9",
+    "url": "/1st-website/static/media/about.4117af08.jfif"
+  },
+  {
+    "revision": "8566f7b0d9d6313004dc9da6509cb683",
+    "url": "/1st-website/static/media/app.8566f7b0.jfif"
+  },
+  {
+    "revision": "5d9b47920eed8563df393f826187d2bc",
+    "url": "/1st-website/static/media/marketing.5d9b4792.jfif"
+  },
+  {
+    "revision": "f0b2718102c0e717236fab48cbe65e26",
+    "url": "/1st-website/static/media/software.f0b27181.jfif"
+  }
+]);
