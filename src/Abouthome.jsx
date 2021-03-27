ffdfb71447626be86ac6266013be8aee9100eb51
kiran stha <kiranstha68@gmail.com>
2021-03-28 00:29:57 +0545

    .

--- src/Abouthome.jsx
+++ src/Abouthome.jsx
@@ -9,28 +9,26 @@ import Experience from "./components/pages/experience/Experience";
 import Skills from "./components/pages/skills/Skills";
 import "./Abouthome.css";
 
-
 const Abouthome = () => {
   return (
     <>
       <section>
-          <div className='wrap-content'>
-              <div className="container">
-              <div className='row sameHeight'>
-          <div className='col s12 m12 l4 light-blue darken-4 sameHeigt_child'>
-            <Profile />
-            <Contact />
-            <Skills />
-          </div>
-          <div className='col s12 m12 l8 white sameHeight_child'>
-            <About />
-            <Education />
-            <Experience />
+        <div className='wrap-content'>
+          <div className='container'>
+            <div className='row sameHeight'>
+              <div className='col s12 m12 l4 light-blue darken-4 sameHeigt_child'>
+                <Profile />
+                <Contact />
+                <Skills />
+              </div>
+              <div className='col s12 m12 l8 white sameHeight_child'>
+                <About />
+                <Education />
+                <Experience />
+              </div>
+            </div>
           </div>
         </div>
-              </div>
-          </div>
-      
       </section>
     </>
   );
